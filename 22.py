f4bbee7a2446beaa19c0c1ab54c9d570712b44b1
Illllp <lukasjohancen@yandex.ru>
2023-05-24 23:59:24 +0300

    Создание списка и изменение с помощью пароля

--- 22.py
+++ 22.py
@@ -1,49 +1,65 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QListView, QLineEdit, QPushButton, QMessageBox
+from tkinter import *
+from tkinter import messagebox
 
-# Define the list of values
-values = ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7', 'value8', 'value9', 'value10']
+password = "12345" # задаем пароль для переписывания списка
 
-# Create the PyQt5 UI
-app = QApplication(sys.argv)
-window = QWidget()
-
-# Create a layout for the UI
-layout = QVBoxLayout()
-
-# Create a label and a QLineEdit for the password
-password_label = QLineEdit("Password:")
-layout.addWidget(password_label)
-password_entry = QLineEdit()
-layout.addWidget(password_entry)
-
-# Create a QListView and populate it with the values
-listview = QListView()
-listview_model = listview.model()
-for value in values:
-    listview_model.insertRow(0)
-    index = listview_model.index(0, 0)
-    listview_model.setData(index, value)
-layout.addWidget(listview)
-
-# Create a button to reverse the list and a function to reverse the list and verify the password
 def reverse_list():
-    password = password_entry.text()
-    if password == 'mypassword':
-        selected_indices = listview.selectedIndexes()
-        selected_values = [listview_model.data(index) for index in selected_indices]
-        for i, index in enumerate(selected_indices):
-            listview_model.setData(index, selected_values[len(selected_values)-i-1])
-    else:
-        QMessageBox.warning(window, "Error", "Incorrect password")
+    # функция для переписывания списка в обратном порядке
+    if password_entry.get() != password:
+        # проверяем правильность введенного пароля
+        messagebox.showerror("Ошибка", "Неправильный пароль!")
+        return
+        
+    # переписываем список в обратном порядке
+    listbox2.delete(0, END)
+    for item in reversed(listbox1.get(0, END)):
+        listbox2.insert(END, item)
+        
+def insert_item():
+    # функция для добавления элемента в список
+    if len(listbox1.get(0, END)) >= 10:
+        # проверяем, что в списке не больше 10 элементов
+        messagebox.showerror("Ошибка", "Список может содержать максимум 10 элементов!")
+        return
+        
+    item = item_entry.get()
+    if item == "":
+        # элемент не может быть пустым
+        messagebox.showerror("Ошибка", "Элемент не может быть пустым!")
+        return
+        
+    # добавляем элемент в список
+    listbox1.insert(END, item)
+    item_entry.delete(0, END)
+    
+# создаем окно приложения
+root = Tk()
+root.title("Список")
 
-reverse_button = QPushButton("Reverse List")
-reverse_button.clicked.connect(reverse_list)
-layout.addWidget(reverse_button)
+# создаем компоненты для добавления элемента в список
+item_label = Label(root, text="Добавить элемент:")
+item_label.grid(row=0, column=0)
+item_entry = Entry(root)
+item_entry.grid(row=0, column=1)
+insert_button = Button(root, text="Добавить", command=insert_item)
+insert_button.grid(row=0, column=2)
 
-# Set the layout for the window and display it
-window.setLayout(layout)
-window.show()
+# создаем компоненты для отображения списка
+listbox1_label = Label(root, text="Список:")
+listbox1_label.grid(row=1, column=0)
+listbox1 = Listbox(root)
+listbox1.grid(row=2, column=0, rowspan=2)
+listbox2_label = Label(root, text="Переписанный список:")
+listbox2_label.grid(row=1, column=2)
+listbox2 = Listbox(root)
+listbox2.grid(row=2, column=2, rowspan=2)
 
-# Start the event loop
-sys.exit(app.exec_())
+# создаем компоненты для переписывания списка
+password_label = Label(root, text="Введите пароль для переписывания списка:")
+password_label.grid(row=4, column=0, columnspan=2)
+password_entry = Entry(root, show="*")
+password_entry.grid(row=5, column=0, columnspan=2)
+reverse_button = Button(root, text="Переписать список в обратном порядке", command=reverse_list)
+reverse_button.grid(row=6, column=0, columnspan=2)
+
+root.mainloop()
