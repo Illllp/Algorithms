58463056fbc436f6ea0b726562ff8dc14e722fb9
Illllp <lukasjohancen@yandex.ru>
2023-05-25 01:29:29 +0300

    объединение 24-25 и 26 лабы

--- 17-18.py
+++ 17-18.py
@@ -1,131 +1,43 @@
-from tkinter import *
-import random
+# /* Square.py
+import pygame
 
-class Checker:
-    def __init__(self, color, position, isKing=False):
-        self.color = color
-        self.position = position
-        self.isKing = isKing
+# Tile creator
+class Square:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.abs_x = x * width
+        self.abs_y = y * height
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.pos = (x, y)
+        self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+        self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
+        self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
+        self.occupying_piece = None
+        self.coord = self.get_coord()
+        self.highlight = False
+        self.rect = pygame.Rect(
+            self.abs_x,
+            self.abs_y,
+            self.width,
+            self.height
+        )
 
-class Color:
-    def __init__(self, red, green, blue):
-        self.red = red
-        self.green = green
-        self.blue = blue
+    # get the formal notation of the tile
+    def get_coord(self):
+        columns = 'abcdefgh'
+        return columns[self.x] + str(self.y + 1)
 
-class Game:
-    def __init__(self):
-        self.active_player = Color(255, 0, 0)
-        self.inactive_player = Color(0, 0, 0)
-        self.board_size = 8
-        self.checker_size = 40
-        self.selected_checker = None
-        self.checkers = {}
-        self.canvas = None
-        self.setup()
-    
-    def setup(self):
-        self.canvas = Canvas(width=self.board_size * self.checker_size, height=self.board_size * self.checker_size)
-        self.canvas.pack()
-        
-        for x in range(self.board_size):
-            for y in range(self.board_size):
-                if (x + y) % 2 == 0:
-                    continue
-                if y < 3:
-                    color = self.inactive_player
-                elif y > 4:
-                    color = self.active_player
-                else:
-                    continue
-                checker = Checker(color, (x, y))
-                self.checkers[(x, y)] = checker
-        
-        self.draw_board()
-        self.draw_checkers()
-    
-    def draw_board(self):
-        for x in range(self.board_size):
-            for y in range(self.board_size):
-                x0 = x * self.checker_size
-                y0 = (7 - y) * self.checker_size
-                x1 = x0 + self.checker_size
-                y1 = y0 + self.checker_size
-                color = "#e6e6e6" if (x + y) % 2 == 0 else "#333333"
-                self.canvas.create_rectangle(x0, y0, x1, y1, fill=color)
-    
-    def draw_checkers(self):
-        for position, checker in self.checkers.items():
-            x0 = position[0] * self.checker_size
-            y0 = (7 - position[1]) * self.checker_size
-            x1 = x0 + self.checker_size
-            y1 = y0 + self.checker_size
-            color = "#{:02x}{:02x}{:02x}".format(checker.color.red, checker.color.green, checker.color.blue)
-            outline = "black" if checker == self.selected_checker else color
-            self.canvas.create_oval(x0, y0, x1, y1, fill=color, outline=outline)
-            if checker.isKing:
-                self.canvas.create_text((x0 + x1) / 2, (y0 + y1) / 2, text="K")
-    
-    def move(self, checker, position):
-        self.checkers[position] = checker
-        del self.checkers[checker.position]
-        checker.position = position
-        if position[1] == 0 and checker.color == self.inactive_player:
-            checker.isKing = True
-        elif position[1] == 7 and checker.color == self.active_player:
-            checker.isKing = True
-        
-    def get_valid_moves(self, checker):
-        x, y = checker.position
-        moves = []
-        jumps = []
-        if checker.isKing or checker.color == self.active_player:
-            for dx in (-1, 1):
-                for dy in (-1, 1):
-                    for dist in range(1, self.board_size):
-                        if x + dx * dist < 0 or x + dx * dist >= self.board_size or y + dy * dist < 0 or y + dy * dist >= self.board_size:
-                            break
-                        position = (x + dx * dist, y + dy * dist)
-                        if position in self.checkers:
-                            break
-                        if self.selected_checker is None or abs(x + dx * dist - self.selected_checker.position[0]) != 2:
-                            moves.append(position)
-                        else:
-                            jumps.append(position)
-                            break
-        if checker.isKing or checker.color == self.inactive_player:
-            for dx in (-1, 1):
-                for dy in (-1, 1):
-                    for dist in range(1, self.board_size):
-                        if x + dx * dist < 0 or x + dx * dist >= self.board_size or y + dy * dist < 0 or y + dy * dist >=self.board_size:
-                            break
-                        position = (x + dx * dist, y + dy * dist)
-                        if position not in self.checkers:
-                            continue
-                        if self.checkers[position].color == checker.color:
-                            break
-                        jump_position = (x + dx * (dist + 1), y + dy * (dist + 1))
-                        if jump_position in self.checkers:
-                            break
-                        jumps.append(jump_position)
-        return jumps if jumps else moves
-    
-    def select(self, position):
-        if position in self.checkers and self.checkers[position].color == self.active_player:
-            self.selected_checker = self.checkers[position]
-        elif position in self.get_valid_moves(self.selected_checker):
-            self.move(self.selected_checker, position)
-            self.selected_checker = None
-            self.active_player, self.inactive_player = self.inactive_player, self.active_player
+    def draw(self, display):
+        # configures if tile should be light or dark or highlighted tile
+        if self.highlight:
+            pygame.draw.rect(display, self.highlight_color, self.rect)
         else:
-            self.selected_checker = None
-        self.canvas.delete("all")
-        self.draw_board()
-        self.draw_checkers()
-    
-    def run(self):
-        self.canvas.bind("<Button-1>", lambda event: self.select((event.x // self.checker_size, 7 - event.y // self.checker_size)))
-        mainloop()
-
-game = Game()
-game.run()
+            pygame.draw.rect(display, self.draw_color, self.rect)
+        # adds the chess piece icons
+        if self.occupying_piece != None:
+            centering_rect = self.occupying_piece.img.get_rect()
+            centering_rect.center = self.rect.center
+            display.blit(self.occupying_piece.img, centering_rect.topleft)
--- 30.py
+++ 30.py
@@ -1,106 +1,23 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsItem
-from PyQt5.QtGui import QBrush, QColor, QPen
-from PyQt5.QtCore import QRectF, Qt
+from tkinter import *
+import os
 
-# Размер ячейки на доске
-CELL_SIZE = 50
+root = Tk()
+root.geometry("200x200")
 
-# Класс Шашка
-class Checker(QGraphicsItem):
-    def __init__(self, color, row, column):
-        super().__init__()
-        self.color = color
-        self.row = row
-        self.column = column
-        self.is_king = False
-        
-    # Функция отрисовки шашки
-    def paint(self, painter, option, widget):
-        rect = QRectF(self.column * CELL_SIZE, self.row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        painter.setBrush(QBrush(self.color))
-        painter.setPen(QPen(Qt.black, 2))
-        painter.drawEllipse(rect)
-        if self.is_king:
-            painter.setBrush(QBrush(QColor(255, 255, 255)))
-            painter.drawEllipse(rect.center(), CELL_SIZE/5, CELL_SIZE/5)
+# Функция для переключения между программами по щелчку на значке
+def switch_program():
+    os.system("taskkill /im notepad.exe") # закрытие запущенной программы
     
-    # Функция возврата ограничивающего прямоугольника
-    def boundingRect(self):
-        return QRectF(self.column * CELL_SIZE, self.row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-    
-    # Функция перемещения шашки
-    def move(self, row, column):
-        self.row = row
-        self.column = column
-        self.setPos(column * CELL_SIZE, row * CELL_SIZE)
-        
-# Класс Игровое поле
-class Board(QGraphicsScene):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.checkers = []
-        self.selected_checker = None
-        self.init_board()
-        
-    # Функция отрисовки доски
-    def draw_board(self):
-        for row in range(8):
-            for column in range(8):
-                rect = QRectF(column * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-                if (row + column) % 2 == 0:
-                    self.addRect(rect, QPen(Qt.black), QBrush(QColor(255, 206, 158)))
-                else:
-                    self.addRect(rect, QPen(Qt.black), QBrush(QColor(209, 139, 71)))
-                    
-    # Функция создания шашек на доске
-    def init_board(self):
-        for i in range(3):
-            for j in range(8):
-                if (i + j) % 2 == 0:
-                    checker = Checker(QColor(255, 0, 0), i, j)
-                    self.addItem(checker)
-                    self.checkers.append(checker)
-                    
-        for i in range(5, 8):
-            for j in range(8):
-                if (i + j) % 2 == 0:
-                    checker = Checker(QColor(0, 0, 255), i, j)
-                    self.addItem(checker)
-                    self.checkers.append(checker)
-                    
-    # Функция выбора шашки для перемещения
-    def select_checker(self, checker):
-        if self.selected_checker:
-            self.selected_checker.setBrush(QBrush(self.selected_checker.color))
-        self.selected_checker = checker
-        self.selected_checker.setBrush(QBrush(QColor(0, 255, 0)))
-            
-    # Функция перемещения шашки
-    def move_checker(self, row, column):
-        if self.selected_checker:
-            self.selected_checker.move(row, column)
-            if row in [0, 7]:
-                self.selected_checker.is_king = True
-            self.selected_checker.setBrush(QBrush(self.selected_checker.color))
-            self.selected_checker = None
-            
-    # Функция клика мыши
-    def mousePressEvent(self, event):
-        pos = event.scenePos()
-        items = self.items(pos.x(), pos.y(), CELL_SIZE, CELL_SIZE)
-        if len(items) > 0 and isinstance(items[0], Checker):
-            self.select_checker(items[0])
-        else:
-            row = int(pos.y() // CELL_SIZE)
-            column = int(pos.x() // CELL_SIZE)
-            self.move_checker(row, column)
+    # проверка, какая программа запущена, и запуск другой программы
+    if btn.cget('text') == "Run Program 1":
+        os.system("notepad")
+        btn.config(text="Run Program 2")
+    else:
+        os.system("calc")
+        btn.config(text="Run Program 1")
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    scene = Board()
-    view = QGraphicsView(scene)
-    view.setWindowTitle('Checkers')
-    view.setFixedSize(CELL_SIZE * 8, CELL_SIZE *8)
-    view.show()
-    sys.exit(app.exec_())
+# Создаем кнопку-значок
+btn = Button(root, text="Run Program 1", command=switch_program, padx=10, pady=10)
+btn.pack(pady=50)
+
+root.mainloop()
